04fa1c91f2a959870e2726316ec61941b9f9530e
StableChen <StableChen@126.com>
2016-12-22 21:06:34 +0800

    Create database and table.

---
+++ app/src/main/java/whut/dongdong/coolweather/db/City.java
@@ -0,0 +1,47 @@
+package whut.dongdong.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dongdong on 2016/12/22.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/whut/dongdong/coolweather/db/County.java
@@ -0,0 +1,47 @@
+package whut.dongdong.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by dongdong on 2016/12/22.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
